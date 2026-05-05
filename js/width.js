07832399fea116cc9c1cd09642416966e5864463
ATTAHIRU AHMAD ALKALI <127088437+Alqalis@users.noreply.github.com>
2026-05-05 13:06:35 +0100

    Delete js directory

--- js/width.js
+++
@@ -1,8 +0,0 @@
-
-window.onload = screen;
-window.onresize = screen;
-
-function screen(){
-    width = window.innerWidth;
-    document.getElementById("size").innerHTML = "size " + width + " PX";
-}
